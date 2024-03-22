2555846f78c65e3af63c32fc1b385b7e13805880
Nox-Matt <andreasmatthew0271@gmail.com>
2024-03-22 17:14:59 +0700

    update-certificate and script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
+
+
+function displayCrf(){
+    fetch('crf.json')
+        .then(response => response.json())
+        .then(data => {
+            const Container = document.getElementById('image-container');
+            const image = data.images;
+
+            image.forEach(imageUrl => {
+                const image = document.createElement('img');
+                image.src = imageUrl;
+                Container.appendChild(image);
+            });
+        })
+        .catch(error => {
+            console.error('Error JSON:', error);
+        });
+    }
